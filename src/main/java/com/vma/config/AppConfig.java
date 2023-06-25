7d1b63338f6f4ab4c93a79b3d44e5cc98f984228
mavoropaev <m.a.voropaev@gmail.com>
2023-06-25 11:50:27 +0300

    1 commit - AppConfig

---
+++ src/main/java/com/vma/config/AppConfig.java
@@ -0,0 +1,57 @@
+package com.vma.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.cfg.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class AppConfig {
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean(){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.vma.domain");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    public Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MqSQLDialect");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.HBM2DDL_AUTO, "validate");
+
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
+        dataSource.setJdbcUrl("jdbc:p6spy:mysql://localhost:3306/project_spring1");
+        dataSource.setUsername("root");
+        dataSource.setPassword("college");
+        dataSource.setMaximumPoolSize(10);
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory managerFactory){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(managerFactory);
+
+        return transactionManager;
+    }
+}
---
+++ src/main/java/com/vma/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.vma.config;
+
+public class WebConfig {
+}
